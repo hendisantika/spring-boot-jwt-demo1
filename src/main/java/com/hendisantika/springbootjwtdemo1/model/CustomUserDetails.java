7c8e8147ea05596667819e6dc11a172f274e79e3
hendisantika <hendisantika@yahoo.co.id>
2023-01-19 06:50:09 +0700

    #28 - Add CustomUserDetails ✏️ 📗 🐳

---
+++ src/main/java/com/hendisantika/springbootjwtdemo1/model/CustomUserDetails.java
@@ -0,0 +1,80 @@
+package com.hendisantika.springbootjwtdemo1.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-demo1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/19/23
+ * Time: 06:49
+ * To change this template use File | Settings | File Templates.
+ */
+public class CustomUserDetails extends User implements UserDetails {
+
+    public CustomUserDetails(final User user) {
+        super(user);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getRole().name()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return super.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return super.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return super.getActive();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return super.getEmailVerified();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        CustomUserDetails that = (CustomUserDetails) obj;
+        return Objects.equals(getId(), that.getId());
+    }
+}
