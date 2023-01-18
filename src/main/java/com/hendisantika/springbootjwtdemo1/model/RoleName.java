3752b86cea3c80a6509a89d526198ef37c545a8f
hendisantika <hendisantika@yahoo.co.id>
2023-01-19 06:52:50 +0700

    #31 - Add RoleName Model ✏️ 📗 🐳

---
+++ src/main/java/com/hendisantika/springbootjwtdemo1/model/RoleName.java
@@ -0,0 +1,23 @@
+package com.hendisantika.springbootjwtdemo1.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-demo1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/19/23
+ * Time: 06:52
+ * To change this template use File | Settings | File Templates.
+ */
+public enum RoleName {
+    /**
+     * Role admin authority role.
+     */
+    ROLE_ADMIN,
+
+    /**
+     * Role user authority role.
+     */
+    ROLE_USER
+}
