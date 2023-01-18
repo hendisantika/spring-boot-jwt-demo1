fd431f466c632318c013703441be0e70e241b1e3
hendisantika <hendisantika@yahoo.co.id>
2023-01-18 12:13:20 +0700

    #1 - Initial commit ✏️ 📗 🐳⬆

---
+++ src/main/java/com/hendisantika/springbootjwtdemo1/SpringBootJwtDemo1Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootjwtdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtDemo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hendisantika/springbootjwtdemo1/SpringBootJwtDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootjwtdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
