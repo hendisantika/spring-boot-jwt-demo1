8525f41d5701e6e070dad46a56449a8aef5d9baa
hendisantika <hendisantika@yahoo.co.id>
2023-01-18 12:29:54 +0700

    #19 - Add DeviceType Enum ✏️ 📗 🐳

---
+++ src/main/java/com/hendisantika/springbootjwtdemo1/model/token/DeviceType.java
@@ -0,0 +1,24 @@
+package com.hendisantika.springbootjwtdemo1.model.token;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-demo1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/18/23
+ * Time: 12:29
+ * To change this template use File | Settings | File Templates.
+ */
+public enum DeviceType {
+
+    /**
+     * Android device type
+     */
+    DEVICE_TYPE_ANDROID,
+
+    /**
+     * IOS device type
+     */
+    DEVICE_TYPE_IOS,
+}
