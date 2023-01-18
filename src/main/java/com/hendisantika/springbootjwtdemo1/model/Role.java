d0f91aece7ce7cf8c361076eb31a19513f92fe17
hendisantika <hendisantika@yahoo.co.id>
2023-01-19 06:52:26 +0700

    #30 - Add Role Model ✏️ 📗 🐳

---
+++ src/main/java/com/hendisantika/springbootjwtdemo1/model/Role.java
@@ -0,0 +1,48 @@
+package com.hendisantika.springbootjwtdemo1.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-demo1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/19/23
+ * Time: 06:51
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity(name = "ROLE")
+@Data
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @Column(name = "ROLE_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "ROLE_NAME")
+    @Enumerated(EnumType.STRING)
+    @NaturalId
+    private RoleName role;
+
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private Set<User> userList = new HashSet<>();
+
+    public Role(RoleName role) {
+        this.role = role;
+    }
+
+    public Role(RoleName role) {
+        this.role = role;
+    }
+}
