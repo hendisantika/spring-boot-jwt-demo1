a2465ec95bbced349a44a463ac73f698ff21606e
hendisantika <hendisantika@yahoo.co.id>
2023-01-19 07:06:51 +0700

    #45 - Add ResourceNotFoundException Exception ✏️ 📗 🐳

---
+++ src/main/java/com/hendisantika/springbootjwtdemo1/exception/ResourceNotFoundException.java
@@ -0,0 +1,41 @@
+package com.hendisantika.springbootjwtdemo1.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-demo1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/19/23
+ * Time: 07:06
+ * To change this template use File | Settings | File Templates.
+ */
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+    private final String resourceName;
+    private final String fieldName;
+    private final Object fieldValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public Object getFieldValue() {
+        return fieldValue;
+    }
+}
