94a78eabaaea47b599747db1bb0ff4294e30d22c
hendisantika <hendisantika@yahoo.co.id>
2023-01-19 07:04:57 +0700

    #40 - Add InvalidTokenRequestException Exception ✏️ 📗 🐳

---
+++ src/main/java/com/hendisantika/springbootjwtdemo1/exception/InvalidTokenRequestException.java
@@ -0,0 +1,29 @@
+package com.hendisantika.springbootjwtdemo1.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-demo1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/19/23
+ * Time: 07:04
+ * To change this template use File | Settings | File Templates.
+ */
+@ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
+public class InvalidTokenRequestException extends RuntimeException {
+
+    private final String tokenType;
+    private final String token;
+    private final String message;
+
+    public InvalidTokenRequestException(String tokenType, String token, String message) {
+        super(String.format("%s: [%s] token: [%s] ", message, tokenType, token));
+        this.tokenType = tokenType;
+        this.token = token;
+        this.message = message;
+    }
+}
