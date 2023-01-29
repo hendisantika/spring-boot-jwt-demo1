1e9fa4ef3343c2c0544b964ce6446d3f300731ff
hendisantika <hendisantika@yahoo.co.id>
2023-01-29 07:16:23 +0700

    #48 - Add UpdatePasswordException ✏️ 📗 :frog:

---
+++ src/main/java/com/hendisantika/springbootjwtdemo1/exception/UpdatePasswordException.java
@@ -0,0 +1,27 @@
+package com.hendisantika.springbootjwtdemo1.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-demo1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/29/23
+ * Time: 07:16
+ * To change this template use File | Settings | File Templates.
+ */
+@ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+public class UpdatePasswordException extends RuntimeException {
+
+    private final String user;
+    private final String message;
+
+    public UpdatePasswordException(String user, String message) {
+        super(String.format("Couldn't update password for [%s]: [%s])", user, message));
+        this.user = user;
+        this.message = message;
+    }
+}
