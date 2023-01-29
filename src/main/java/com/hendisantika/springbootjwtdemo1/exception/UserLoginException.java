0df1775b47ef90fe870575ff180319873a441a90
hendisantika <hendisantika@yahoo.co.id>
2023-01-29 07:16:42 +0700

    #49 - Add UserLoginException ✏️ 📗 :frog:

---
+++ src/main/java/com/hendisantika/springbootjwtdemo1/exception/UserLoginException.java
@@ -0,0 +1,26 @@
+package com.hendisantika.springbootjwtdemo1.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-demo1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/29/23
+ * Time: 07:16
+ * To change this template use File | Settings | File Templates.
+ */
+@ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+public class UserLoginException extends RuntimeException {
+
+    public UserLoginException(String message) {
+        super(message);
+    }
+
+    public UserLoginException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
