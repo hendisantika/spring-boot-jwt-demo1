3567b9bbf3d759441ec31edf65544eb93f687b46
hendisantika <hendisantika@yahoo.co.id>
2023-02-13 09:21:05 +0700

    #74 - Add markLogoutEventForToken in LoggedOutJwtTokenCache ✏️ 📗 🐳⬆

--- src/main/java/com/hendisantika/springbootjwtdemo1/cache/LoggedOutJwtTokenCache.java
+++ src/main/java/com/hendisantika/springbootjwtdemo1/cache/LoggedOutJwtTokenCache.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-jwt-demo1
@@ -31,5 +34,20 @@ public class LoggedOutJwtTokenCache {
                 .variableExpiration()
                 .maxSize(maxSize)
                 .build();
+    }
+
+    public void markLogoutEventForToken(OnUserLogoutSuccessEvent event) {
+        String token = event.getToken();
+        if (tokenEventMap.containsKey(token)) {
+            logger.info(String.format("Log out token for user [%s] is already present in the cache",
+                    event.getUserEmail()));
+
+        } else {
+            Date tokenExpiryDate = tokenProvider.getTokenExpiryFromJWT(token);
+            long ttlForToken = getTTLForToken(tokenExpiryDate);
+            logger.info(String.format("Logout token cache set for [%s] with a TTL of [%s] seconds. Token is due " +
+                    "expiry at [%s]", event.getUserEmail(), ttlForToken, tokenExpiryDate));
+            tokenEventMap.put(token, event, ttlForToken, TimeUnit.SECONDS);
+        }
     }
 }
