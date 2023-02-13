3dc97efb840460a7938b8c6db69f73edd6903d6b
hendisantika <hendisantika@yahoo.co.id>
2023-02-13 09:25:11 +0700

    #80 - Add OnUserAccountChangeEvent ✏️ 📗 🐳⬆

---
+++ src/main/java/com/hendisantika/springbootjwtdemo1/event/listener/OnUserAccountChangeEvent.java
@@ -0,0 +1,51 @@
+package com.hendisantika.springbootjwtdemo1.event.listener;
+
+import org.springframework.context.ApplicationEvent;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-demo1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2/13/23
+ * Time: 09:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class OnUserAccountChangeEvent extends ApplicationEvent {
+
+    private User user;
+    private String action;
+    private String actionStatus;
+
+    public OnUserAccountChangeEvent(User user, String action, String actionStatus) {
+        super(user);
+        this.user = user;
+        this.action = action;
+        this.actionStatus = actionStatus;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getActionStatus() {
+        return actionStatus;
+    }
+
+    public void setActionStatus(String actionStatus) {
+        this.actionStatus = actionStatus;
+    }
+}
