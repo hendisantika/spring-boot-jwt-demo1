73fc62172c083ceadcddd0039d80e6f0216bf524
hendisantika <hendisantika@yahoo.co.id>
2023-02-13 09:30:07 +0700

    #90 - Add sendAccountChangeEmail in OnUserAccountChangeListener ✏️ 📗 🐳⬆

--- src/main/java/com/hendisantika/springbootjwtdemo1/event/listener/OnUserAccountChangeListener.java
+++ src/main/java/com/hendisantika/springbootjwtdemo1/event/listener/OnUserAccountChangeListener.java
@@ -1,11 +1,17 @@
 package com.hendisantika.springbootjwtdemo1.event.listener;
 
 import com.hendisantika.springbootjwtdemo1.event.OnUserAccountChangeEvent;
+import com.hendisantika.springbootjwtdemo1.exception.MailSendException;
+import com.hendisantika.springbootjwtdemo1.model.User;
+import freemarker.template.TemplateException;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -36,5 +42,22 @@ public class OnUserAccountChangeListener implements ApplicationListener<OnUserAc
     @Async
     public void onApplicationEvent(OnUserAccountChangeEvent onUserAccountChangeEvent) {
         sendAccountChangeEmail(onUserAccountChangeEvent);
+    }
+
+    /**
+     * Send email verification to the user and persist the token in the database.
+     */
+    private void sendAccountChangeEmail(OnUserAccountChangeEvent event) {
+        User user = event.getUser();
+        String action = event.getAction();
+        String actionStatus = event.getActionStatus();
+        String recipientAddress = user.getEmail();
+
+        try {
+            mailService.sendAccountChangeEmail(action, actionStatus, recipientAddress);
+        } catch (IOException | TemplateException | MessagingException e) {
+            logger.error(e);
+            throw new MailSendException(recipientAddress, "Account Change Mail");
+        }
     }
 }
