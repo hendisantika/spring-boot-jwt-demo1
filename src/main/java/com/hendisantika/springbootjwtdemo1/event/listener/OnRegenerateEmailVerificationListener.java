0320f11a96350a9ce958d831b5e1b2ae1614dce9
hendisantika <hendisantika@yahoo.co.id>
2023-02-13 09:27:42 +0700

    #85 - Add OnRegenerateEmailVerificationListener ✏️ 📗 🐳⬆

---
+++ src/main/java/com/hendisantika/springbootjwtdemo1/event/listener/OnRegenerateEmailVerificationListener.java
@@ -0,0 +1,29 @@
+package com.hendisantika.springbootjwtdemo1.event.listener;
+
+import com.hendisantika.springbootjwtdemo1.event.OnRegenerateEmailVerificationEvent;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-demo1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2/13/23
+ * Time: 09:27
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+public class OnRegenerateEmailVerificationListener implements ApplicationListener<OnRegenerateEmailVerificationEvent> {
+
+    private static final Logger logger = Logger.getLogger(OnRegenerateEmailVerificationListener.class);
+    private final MailService mailService;
+
+    @Autowired
+    public OnRegenerateEmailVerificationListener(MailService mailService) {
+        this.mailService = mailService;
+    }
+}
