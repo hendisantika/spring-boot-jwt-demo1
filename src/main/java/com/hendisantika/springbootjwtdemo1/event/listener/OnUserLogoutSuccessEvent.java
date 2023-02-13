d734c482d9c29020a1acaa845b7bedcb63277ba6
hendisantika <hendisantika@yahoo.co.id>
2023-02-13 09:22:50 +0700

    #77 - Add OnUserLogoutSuccessEvent ✏️ 📗 🐳⬆

---
+++ src/main/java/com/hendisantika/springbootjwtdemo1/event/listener/OnUserLogoutSuccessEvent.java
@@ -0,0 +1,35 @@
+package com.hendisantika.springbootjwtdemo1.event.listener;
+
+import com.hendisantika.springbootjwtdemo1.model.payload.LogOutRequest;
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
+
+import java.time.Instant;
+import java.util.Date;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-demo1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2/13/23
+ * Time: 09:22
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+public class OnUserLogoutSuccessEvent extends ApplicationEvent {
+
+    private final String userEmail;
+    private final String token;
+    private final transient LogOutRequest logOutRequest;
+    private final Date eventTime;
+
+    public OnUserLogoutSuccessEvent(String userEmail, String token, LogOutRequest logOutRequest) {
+        super(userEmail);
+        this.userEmail = userEmail;
+        this.token = token;
+        this.logOutRequest = logOutRequest;
+        this.eventTime = Date.from(Instant.now());
+    }
+}
