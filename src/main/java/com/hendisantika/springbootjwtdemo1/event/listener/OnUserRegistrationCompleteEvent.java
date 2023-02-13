a49eaee3a0cd5bb20e3f000911d5b1408a7ff3e6
hendisantika <hendisantika@yahoo.co.id>
2023-02-13 09:25:32 +0700

    #81 - Add OnUserRegistrationCompleteEvent ✏️ 📗 🐳⬆

---
+++ src/main/java/com/hendisantika/springbootjwtdemo1/event/listener/OnUserRegistrationCompleteEvent.java
@@ -0,0 +1,42 @@
+package com.hendisantika.springbootjwtdemo1.event.listener;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.web.util.UriComponentsBuilder;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-demo1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2/13/23
+ * Time: 09:25
+ * To change this template use File | Settings | File Templates.
+ */
+public class OnUserRegistrationCompleteEvent extends ApplicationEvent {
+
+    private transient UriComponentsBuilder redirectUrl;
+    private User user;
+
+    public OnUserRegistrationCompleteEvent(User user, UriComponentsBuilder redirectUrl) {
+        super(user);
+        this.user = user;
+        this.redirectUrl = redirectUrl;
+    }
+
+    public UriComponentsBuilder getRedirectUrl() {
+        return redirectUrl;
+    }
+
+    public void setRedirectUrl(UriComponentsBuilder redirectUrl) {
+        this.redirectUrl = redirectUrl;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
