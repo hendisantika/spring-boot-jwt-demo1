081f80421028940d535d92601240f642c6d7e922
hendisantika <hendisantika@yahoo.co.id>
2023-02-13 09:23:55 +0700

    #78 - Add OnGenerateResetLinkEvent ✏️ 📗 🐳⬆

---
+++ src/main/java/com/hendisantika/springbootjwtdemo1/event/listener/OnGenerateResetLinkEvent.java
@@ -0,0 +1,43 @@
+package com.hendisantika.springbootjwtdemo1.event.listener;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.web.util.UriComponentsBuilder;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-demo1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2/13/23
+ * Time: 09:23
+ * To change this template use File | Settings | File Templates.
+ */
+public class OnGenerateResetLinkEvent extends ApplicationEvent {
+
+    private transient UriComponentsBuilder redirectUrl;
+
+    private transient PasswordResetToken passwordResetToken;
+
+    public OnGenerateResetLinkEvent(PasswordResetToken passwordResetToken, UriComponentsBuilder redirectUrl) {
+        super(passwordResetToken);
+        this.passwordResetToken = passwordResetToken;
+        this.redirectUrl = redirectUrl;
+    }
+
+    public PasswordResetToken getPasswordResetToken() {
+        return passwordResetToken;
+    }
+
+    public void setPasswordResetToken(PasswordResetToken passwordResetToken) {
+        this.passwordResetToken = passwordResetToken;
+    }
+
+    public UriComponentsBuilder getRedirectUrl() {
+        return redirectUrl;
+    }
+
+    public void setRedirectUrl(UriComponentsBuilder redirectUrl) {
+        this.redirectUrl = redirectUrl;
+    }
+}
