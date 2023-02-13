d3257ccd1cd193da032300811a08199eba7d9bc3
hendisantika <hendisantika@yahoo.co.id>
2023-02-13 09:27:02 +0700

    #84 - Add sendResetLink in OnGenerateResetLinkEventListener ✏️ 📗 🐳⬆

--- src/main/java/com/hendisantika/springbootjwtdemo1/event/listener/OnGenerateResetLinkEventListener.java
+++ src/main/java/com/hendisantika/springbootjwtdemo1/event/listener/OnGenerateResetLinkEventListener.java
@@ -1,11 +1,15 @@
 package com.hendisantika.springbootjwtdemo1.event.listener;
 
 import com.hendisantika.springbootjwtdemo1.event.OnGenerateResetLinkEvent;
+import freemarker.template.TemplateException;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -36,5 +40,22 @@ public class OnGenerateResetLinkEventListener implements ApplicationListener<OnG
     @Async
     public void onApplicationEvent(OnGenerateResetLinkEvent onGenerateResetLinkMailEvent) {
         sendResetLink(onGenerateResetLinkMailEvent);
+    }
+
+    /**
+     * Sends Reset Link to the mail address with a password reset link token
+     */
+    private void sendResetLink(OnGenerateResetLinkEvent event) {
+        PasswordResetToken passwordResetToken = event.getPasswordResetToken();
+        User user = passwordResetToken.getUser();
+        String recipientAddress = user.getEmail();
+        String emailConfirmationUrl = event.getRedirectUrl().queryParam("token", passwordResetToken.getToken())
+                .toUriString();
+        try {
+            mailService.sendResetLink(emailConfirmationUrl, recipientAddress);
+        } catch (IOException | TemplateException | MessagingException e) {
+            logger.error(e);
+            throw new MailSendException(recipientAddress, "Email Verification");
+        }
     }
 }
