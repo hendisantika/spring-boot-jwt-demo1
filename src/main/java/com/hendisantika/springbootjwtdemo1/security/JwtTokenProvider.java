e34e91de0c0d729c8f99d81a042ca951ac902523
hendisantika <hendisantika@yahoo.co.id>
2023-02-15 09:54:08 +0700

    #111 - Add generateToken Security ✏️ 📗 🐳

--- src/main/java/com/hendisantika/springbootjwtdemo1/security/JwtTokenProvider.java
+++ src/main/java/com/hendisantika/springbootjwtdemo1/security/JwtTokenProvider.java
@@ -1,7 +1,13 @@
 package com.hendisantika.springbootjwtdemo1.security;
 
+import com.hendisantika.springbootjwtdemo1.model.CustomUserDetails;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.Date;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,5 +30,21 @@ public class JwtTokenProvider {
                             @Value("${app.jwt.expiration}") long jwtExpirationInMs) {
         this.jwtSecret = jwtSecret;
         this.jwtExpirationInMs = jwtExpirationInMs;
+    }
+
+    /**
+     * Generates a token from a principal object. Embed the refresh token in the jwt
+     * so that a new jwt can be created
+     */
+    public String generateToken(CustomUserDetails customUserDetails) {
+        Instant expiryDate = Instant.now().plusMillis(jwtExpirationInMs);
+        String authorities = getUserAuthorities(customUserDetails);
+        return Jwts.builder()
+                .setSubject(Long.toString(customUserDetails.getId()))
+                .setIssuedAt(Date.from(Instant.now()))
+                .setExpiration(Date.from(expiryDate))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .claim(AUTHORITIES_CLAIM, authorities)
+                .compact();
     }
 }
