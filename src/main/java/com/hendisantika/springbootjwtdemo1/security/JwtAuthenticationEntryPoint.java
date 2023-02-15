4a85056402e873cf0008843eeca2df0720defaf2
hendisantika <hendisantika@yahoo.co.id>
2023-02-15 10:06:06 +0700

    #124 - Add commence in JwtAuthenticationEntryPoint ✏️ 📗 🐳

--- src/main/java/com/hendisantika/springbootjwtdemo1/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/hendisantika/springbootjwtdemo1/security/JwtAuthenticationEntryPoint.java
@@ -3,9 +3,14 @@ package com.hendisantika.springbootjwtdemo1.security;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,5 +31,19 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Autowired
     public JwtAuthenticationEntryPoint(@Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
         this.resolver = resolver;
+    }
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse httpServletResponse,
+                         AuthenticationException ex) throws IOException {
+        logger.error("User is unauthorised. Routing from the entry point");
+
+        if (request.getAttribute("javax.servlet.error.exception") != null) {
+            Throwable throwable = (Throwable) request.getAttribute("javax.servlet.error.exception");
+            resolver.resolveException(request, httpServletResponse, null, (Exception) throwable);
+        }
+        if (!httpServletResponse.isCommitted()) {
+            httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, ex.getMessage());
+        }
     }
 }
