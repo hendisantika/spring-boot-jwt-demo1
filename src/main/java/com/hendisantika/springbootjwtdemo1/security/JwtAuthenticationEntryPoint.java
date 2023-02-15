26153a8c62899891b4527fdedf1c98e4c3ef0875
hendisantika <hendisantika@yahoo.co.id>
2023-02-15 10:05:46 +0700

    #123 - Add JwtAuthenticationEntryPoint ✏️ 📗 🐳

---
+++ src/main/java/com/hendisantika/springbootjwtdemo1/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,30 @@
+package com.hendisantika.springbootjwtdemo1.security;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-demo1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2/15/23
+ * Time: 10:05
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private static final Logger logger = Logger.getLogger(JwtAuthenticationEntryPoint.class);
+    private final HandlerExceptionResolver resolver;
+
+    @Autowired
+    public JwtAuthenticationEntryPoint(@Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
+        this.resolver = resolver;
+    }
+}
