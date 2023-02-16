22be5cb7c281891bfd206ad5ffb11742afd9f7fd
hendisantika <hendisantika@yahoo.co.id>
2023-02-17 06:25:27 +0700

    #157 - Add findByToken in RefreshTokenService ✏️ 📗 🐳⬆

--- src/main/java/com/hendisantika/springbootjwtdemo1/service/RefreshTokenService.java
+++ src/main/java/com/hendisantika/springbootjwtdemo1/service/RefreshTokenService.java
@@ -1,10 +1,13 @@
 package com.hendisantika.springbootjwtdemo1.service;
 
+import com.hendisantika.springbootjwtdemo1.model.token.RefreshToken;
 import com.hendisantika.springbootjwtdemo1.repository.RefreshTokenRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,4 +29,10 @@ public class RefreshTokenService {
     @Value("${app.token.refresh.duration}")
     private Long refreshTokenDurationMs;
 
+    /**
+     * Find a refresh token based on the natural id i.e the token itself
+     */
+    public Optional<RefreshToken> findByToken(String token) {
+        return refreshTokenRepository.findByToken(token);
+    }
 }
