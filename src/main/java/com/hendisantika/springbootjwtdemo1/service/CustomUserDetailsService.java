057e922690310b73ce94e683273d6610741de3bb
hendisantika <hendisantika@yahoo.co.id>
2023-02-17 06:20:43 +0700

    #147 - Add loadUserByUsername in CustomUserDetailsService ✏️ 📗 🐳⬆

--- src/main/java/com/hendisantika/springbootjwtdemo1/service/CustomUserDetailsService.java
+++ src/main/java/com/hendisantika/springbootjwtdemo1/service/CustomUserDetailsService.java
@@ -1,10 +1,15 @@
 package com.hendisantika.springbootjwtdemo1.service;
 
+import com.hendisantika.springbootjwtdemo1.model.User;
 import com.hendisantika.springbootjwtdemo1.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,5 +28,12 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> dbUser = userRepository.findByEmail(email);
+        log.info("Fetched user : " + dbUser + " by " + email);
+        return dbUser.map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("Couldn't find a matching user email in the database" +
+                        " for " + email));
+    }
 }
