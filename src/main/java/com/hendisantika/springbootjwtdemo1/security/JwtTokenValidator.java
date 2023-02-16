b63ad9cac98f4495d9bc935f7053a6740e939312
hendisantika <hendisantika@yahoo.co.id>
2023-02-16 18:37:24 +0700

    #129 - Add validateToken in JwtTokenValidator ✏️ 📗 🐳⬆

--- src/main/java/com/hendisantika/springbootjwtdemo1/security/JwtTokenValidator.java
+++ src/main/java/com/hendisantika/springbootjwtdemo1/security/JwtTokenValidator.java
@@ -1,6 +1,12 @@
 package com.hendisantika.springbootjwtdemo1.security;
 
 import com.hendisantika.springbootjwtdemo1.cache.LoggedOutJwtTokenCache;
+import com.hendisantika.springbootjwtdemo1.exception.InvalidTokenRequestException;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -27,5 +33,40 @@ public class JwtTokenValidator {
     public JwtTokenValidator(@Value("${app.jwt.secret}") String jwtSecret, LoggedOutJwtTokenCache loggedOutTokenCache) {
         this.jwtSecret = jwtSecret;
         this.loggedOutTokenCache = loggedOutTokenCache;
+    }
+
+    /**
+     * Validates if a token satisfies the following properties
+     * - Signature is not malformed
+     * - Token hasn't expired
+     * - Token is supported
+     * - Token has not recently been logged out.
+     */
+    public boolean validateToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+
+        } catch (SignatureException ex) {
+            logger.error("Invalid JWT signature");
+            throw new InvalidTokenRequestException("JWT", authToken, "Incorrect signature");
+
+        } catch (MalformedJwtException ex) {
+            logger.error("Invalid JWT token");
+            throw new InvalidTokenRequestException("JWT", authToken, "Malformed jwt token");
+
+        } catch (ExpiredJwtException ex) {
+            logger.error("Expired JWT token");
+            throw new InvalidTokenRequestException("JWT", authToken, "Token expired. Refresh required");
+
+        } catch (UnsupportedJwtException ex) {
+            logger.error("Unsupported JWT token");
+            throw new InvalidTokenRequestException("JWT", authToken, "Unsupported JWT token");
+
+        } catch (IllegalArgumentException ex) {
+            logger.error("JWT claims string is empty.");
+            throw new InvalidTokenRequestException("JWT", authToken, "Illegal argument token");
+        }
+        validateTokenIsNotForALoggedOutDevice(authToken);
+        return true;
     }
 }
