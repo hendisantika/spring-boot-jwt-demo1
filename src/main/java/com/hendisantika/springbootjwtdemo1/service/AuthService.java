514ffae11c1a8ef2c346287404347a84e7a88fa9
hendisantika <hendisantika@yahoo.co.id>
2023-02-16 18:39:03 +0700

    #132 - Add registerUser in AuthService ✏️ 📗 🐳⬆

--- src/main/java/com/hendisantika/springbootjwtdemo1/service/AuthService.java
+++ src/main/java/com/hendisantika/springbootjwtdemo1/service/AuthService.java
@@ -1,10 +1,16 @@
 package com.hendisantika.springbootjwtdemo1.service;
 
+import com.hendisantika.springbootjwtdemo1.exception.ResourceAlreadyInUseException;
+import com.hendisantika.springbootjwtdemo1.model.User;
+import com.hendisantika.springbootjwtdemo1.model.payload.RegistrationRequest;
 import com.hendisantika.springbootjwtdemo1.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.apache.log4j.Logger;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,6 +23,7 @@ import org.springframework.stereotype.Service;
  * To change this template use File | Settings | File Templates.
  */
 @Service
+@RequiredArgsConstructor
 public class AuthService {
 
     private static final Logger logger = Logger.getLogger(AuthService.class);
@@ -28,4 +35,21 @@ public class AuthService {
     private final EmailVerificationTokenService emailVerificationTokenService;
     private final UserDeviceService userDeviceService;
     private final PasswordResetTokenService passwordResetService;
+
+    /**
+     * Registers a new user in the database by performing a series of quick checks.
+     *
+     * @return A user object if successfully created
+     */
+    public Optional<User> registerUser(RegistrationRequest newRegistrationRequest) {
+        String newRegistrationRequestEmail = newRegistrationRequest.getEmail();
+        if (emailAlreadyExists(newRegistrationRequestEmail)) {
+            logger.error("Email already exists: " + newRegistrationRequestEmail);
+            throw new ResourceAlreadyInUseException("Email", "Address", newRegistrationRequestEmail);
+        }
+        logger.info("Trying to register new user [" + newRegistrationRequestEmail + "]");
+        User newUser = userService.createUser(newRegistrationRequest);
+        User registeredNewUser = userService.save(newUser);
+        return Optional.ofNullable(registeredNewUser);
+    }
 }
