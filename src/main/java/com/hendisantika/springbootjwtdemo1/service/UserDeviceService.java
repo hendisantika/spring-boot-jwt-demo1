4e7b7236280d0159338bd82f4215102e41785b9e
hendisantika <hendisantika@yahoo.co.id>
2023-02-18 06:31:53 +0700

    #165 - Add UserDeviceService ✏️ 📗 🐳⬆

---
+++ src/main/java/com/hendisantika/springbootjwtdemo1/service/UserDeviceService.java
@@ -0,0 +1,21 @@
+package com.hendisantika.springbootjwtdemo1.service;
+
+import com.hendisantika.springbootjwtdemo1.repository.UserDeviceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-demo1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2/18/23
+ * Time: 06:31
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class UserDeviceService {
+    private final UserDeviceRepository userDeviceRepository;
+}
