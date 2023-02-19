df8f2410cd9dd434fffb29609c2b0371d88305a9
hendisantika <hendisantika@yahoo.co.id>
2023-02-19 20:05:10 +0700

    #190 - Add configure in WebSecurityConfigurerAdapter ✏️ 📗 🐳⬆

--- src/main/java/com/hendisantika/springbootjwtdemo1/config/WebSecurityConfig.java
+++ src/main/java/com/hendisantika/springbootjwtdemo1/config/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -54,4 +55,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new JwtAuthenticationFilter();
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
 }
