4728c42506152f22aaa6a86e347db4bf0f5c3fd8
hendisantika <hendisantika@yahoo.co.id>
2023-02-19 20:01:57 +0700

    #183 - Add MailConfig ✏️ 📗 🐳⬆

---
+++ src/main/java/com/hendisantika/springbootjwtdemo1/config/MailConfig.java
@@ -0,0 +1,50 @@
+package com.hendisantika.springbootjwtdemo1.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-demo1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2/19/23
+ * Time: 20:01
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@PropertySource("classpath:mail.properties")
+@EnableAsync
+public class MailConfig {
+
+    @Value("${spring.mail.default-encoding}")
+    private String mailDefaultEncoding;
+
+    @Value("${spring.mail.host}")
+    private String mailHost;
+
+    @Value("${spring.mail.username}")
+    private String mailUsername;
+
+    @Value("${spring.mail.password}")
+    private String mailPassword;
+
+    @Value("${spring.mail.port}")
+    private Integer mailPort;
+
+    @Value("${spring.mail.protocol}")
+    private String mailProtocol;
+
+    @Value("${spring.mail.debug}")
+    private String mailDebug;
+
+    @Value("${spring.mail.smtp.auth}")
+    private String mailSmtpAuth;
+
+    @Value("${spring.mail.smtp.starttls.enable}")
+    private String mailSmtpStartTls;
+
+}
