b93821f37fed2fd7cff515dd910a6aa7dc3a026b
hendisantika <hendisantika@yahoo.co.id>
2023-02-19 20:04:22 +0700

    #187 - Add WebSecurityConfigurerAdapter ✏️ 📗 🐳⬆

---
+++ src/main/java/com/hendisantika/springbootjwtdemo1/config/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.hendisantika.springbootjwtdemo1.config;
+
+import com.hendisantika.springbootjwtdemo1.security.JwtAuthenticationEntryPoint;
+import com.hendisantika.springbootjwtdemo1.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-demo1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2/19/23
+ * Time: 20:03
+ * To change this template use File | Settings | File Templates.
+ */
+@Profile("!dev")
+@Configuration
+@EnableWebSecurity(debug = true)
+@EnableJpaRepositories(basePackages = "com.hendisantika.springbootjwtdemo1.repository")
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomUserDetailsService userDetailsService;
+
+    private final JwtAuthenticationEntryPoint jwtEntryPoint;
+
+    @Autowired
+    public WebSecurityConfig(CustomUserDetailsService userDetailsService, JwtAuthenticationEntryPoint jwtEntryPoint) {
+        this.userDetailsService = userDetailsService;
+        this.jwtEntryPoint = jwtEntryPoint;
+    }
+}
