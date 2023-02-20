71f7672fd6e3501a8bf9fed956cbc1edbd28ff78
hendisantika <hendisantika@yahoo.co.id>
2023-02-21 04:08:47 +0700

    #196 - Add checkEmailInUse in AuthController  ✏️ 📗 🐳⬆

--- src/main/java/com/hendisantika/springbootjwtdemo1/controller/AuthController.java
+++ src/main/java/com/hendisantika/springbootjwtdemo1/controller/AuthController.java
@@ -1,12 +1,18 @@
 package com.hendisantika.springbootjwtdemo1.controller;
 
+import com.hendisantika.springbootjwtdemo1.model.payload.ApiResponse;
 import com.hendisantika.springbootjwtdemo1.security.JwtTokenProvider;
 import com.hendisantika.springbootjwtdemo1.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -31,4 +37,11 @@ public class AuthController {
     private final AuthService authService;
     private final JwtTokenProvider tokenProvider;
     private final ApplicationEventPublisher applicationEventPublisher;
+
+    @Operation(summary = "Checks if the given email is in use")
+    @GetMapping("/checkEmailInUse")
+    public ResponseEntity checkEmailInUse(@Param(value = "Email id to check against") @RequestParam("email") String email) {
+        Boolean emailExists = authService.emailAlreadyExists(email);
+        return ResponseEntity.ok(new ApiResponse(true, emailExists.toString()));
+    }
 }
