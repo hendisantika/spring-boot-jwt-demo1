472dc51f6d5ad7229b47724603a030588bb689dd
hendisantika <hendisantika@yahoo.co.id>
2023-02-21 04:13:35 +0700

    #206 - Add getUserProfile in UserController  ✏️ 📗 🐳⬆

--- src/main/java/com/hendisantika/springbootjwtdemo1/controller/UserController.java
+++ src/main/java/com/hendisantika/springbootjwtdemo1/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.hendisantika.springbootjwtdemo1.controller;
 
+import com.hendisantika.springbootjwtdemo1.annotation.CurrentUser;
+import com.hendisantika.springbootjwtdemo1.model.CustomUserDetails;
 import com.hendisantika.springbootjwtdemo1.service.AuthService;
 import com.hendisantika.springbootjwtdemo1.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,4 +39,15 @@ public class UserController {
     private final UserService userService;
 
     private final ApplicationEventPublisher applicationEventPublisher;
+
+    /**
+     * Gets the current user profile of the logged in user
+     */
+    @GetMapping("/me")
+    @PreAuthorize("hasRole('USER')")
+    @Operation(summary = "Returns the current user profile")
+    public ResponseEntity getUserProfile(@CurrentUser CustomUserDetails currentUser) {
+        logger.info(currentUser.getEmail() + " has role: " + currentUser.getRoles());
+        return ResponseEntity.ok("Hello. This is about me");
+    }
 }
