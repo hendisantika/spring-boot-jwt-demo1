a268a572875ff78bac1c9b619cc4bf36f16f4ba9
hendisantika <hendisantika@yahoo.co.id>
2023-02-21 04:10:38 +0700

    #198 - Add authenticateUser in AuthController  ✏️ 📗 🐳⬆

--- src/main/java/com/hendisantika/springbootjwtdemo1/controller/AuthController.java
+++ src/main/java/com/hendisantika/springbootjwtdemo1/controller/AuthController.java
@@ -1,6 +1,11 @@
 package com.hendisantika.springbootjwtdemo1.controller;
 
+import com.hendisantika.springbootjwtdemo1.exception.UserLoginException;
+import com.hendisantika.springbootjwtdemo1.model.CustomUserDetails;
 import com.hendisantika.springbootjwtdemo1.model.payload.ApiResponse;
+import com.hendisantika.springbootjwtdemo1.model.payload.JwtAuthenticationResponse;
+import com.hendisantika.springbootjwtdemo1.model.payload.LoginRequest;
+import com.hendisantika.springbootjwtdemo1.model.token.RefreshToken;
 import com.hendisantika.springbootjwtdemo1.security.JwtTokenProvider;
 import com.hendisantika.springbootjwtdemo1.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,10 +15,16 @@ import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * Created by IntelliJ IDEA.
@@ -57,5 +68,29 @@ public class AuthController {
             "username") String username) {
         Boolean usernameExists = authService.usernameAlreadyExists(username);
         return ResponseEntity.ok(new ApiResponse(true, usernameExists.toString()));
+    }
+
+    /**
+     * Entry point for the user log in. Return the jwt auth token and the refresh token
+     */
+    @PostMapping("/login")
+    @Operation(summary = "Logs the user in to the system and return the auth tokens")
+    public ResponseEntity authenticateUser(@Param(value = "The LoginRequest payload") @Valid @RequestBody LoginRequest loginRequest) {
+
+        Authentication authentication = authService.authenticateUser(loginRequest)
+                .orElseThrow(() -> new UserLoginException("Couldn't login user [" + loginRequest + "]"));
+
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+        logger.info("Logged in User returned [API]: " + customUserDetails.getUsername());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return authService.createAndPersistRefreshTokenForDevice(authentication, loginRequest)
+                .map(RefreshToken::getToken)
+                .map(refreshToken -> {
+                    String jwtToken = authService.generateToken(customUserDetails);
+                    return ResponseEntity.ok(new JwtAuthenticationResponse(jwtToken, refreshToken,
+                            tokenProvider.getExpiryDuration()));
+                })
+                .orElseThrow(() -> new UserLoginException("Couldn't create refresh token for: [" + loginRequest + "]"));
     }
 }
